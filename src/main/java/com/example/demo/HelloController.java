afc647710edf2ff4be7d37c0149d6788d6576c26
LUU CAO CUONG <luucaocuong@Mac.lan>
2026-03-04 20:16:40 +0900

    test1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from ECS CI/CD POC!";
+        return "Hello from ECS CI/CD POC!123";
     }
 }
